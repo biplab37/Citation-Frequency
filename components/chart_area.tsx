3c3c1fe99bbecaf2d24e81684505c2edac394b20
biplab37 <biplabmahato37@gmail.com>
2025-12-30 00:31:31 +0100

    added y axus

--- components/chart_area.tsx
+++ components/chart_area.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { BarChart, Bar, CartesianGrid, XAxis } from "recharts";
+import { BarChart, Bar, CartesianGrid, XAxis, YAxis } from "recharts";
 import { ChartConfig, ChartContainer, ChartTooltip } from "./ui/chart";
 import { DataPoint } from "./search_area";
 import { Card, CardContent, CardHeader, CardTitle } from "./ui/card";
@@ -19,15 +19,16 @@ type ChartAreaProps = {
 export default function ChartArea({ data, keyword }: ChartAreaProps & { keyword: string }) {
     return (
         // <div className="max-w-4xl mx-auto border border-gray-300 h-80 rounded-2xl overflow-hidden shadow-lg shadow-green-600/20">
-        <Card className="mt-4 md:flex w-sm md:w-lg lg:w-4xl">
+        <Card className="mt-8 md:flex w-sm md:w-lg lg:w-4xl">
             <CardHeader>
                 <CardTitle>Number of papers with the word <span className="text-green-600">{keyword}</span> over the year</CardTitle>
             </CardHeader>
             <CardContent>
                 <ChartContainer config={chartConfig} className="min-h-[20vw] min-w-fit">
                     <BarChart accessibilityLayer data={data} className="">
-                        <CartesianGrid />
+                        <CartesianGrid vertical={true}/>
                         <XAxis dataKey="year" />
+                        <YAxis label={{ value: "Number of papers", angle: -90, position: "insideLeft" }} />
                         <ChartTooltip cursor={false} />
                         <Bar dataKey="count" fill="var(--chart-2)" radius={4} />
                     </BarChart>
